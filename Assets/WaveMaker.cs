4de7e0e5dbd89f4255d70f5e9d1cbb1018cfd93d
John Turner <git@norgg.org>
2017-01-20 20:02:00 +0000

    Initial version.

---
+++ Assets/WaveMaker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveMaker : MonoBehaviour {
+
+	float waveProgress = 0.0f;
+
+	// Use this for initialization
+	void Start() {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray)) {
+				StartWave(transform.position);
+			}
+		}
+	}
+
+	void StartWave(Vector3 pos) {
+	}
+}
