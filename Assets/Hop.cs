c9e77ac4413b7a3d10691a705f7699735d9cb9b6
John Turner <git@norgg.org>
2017-01-20 21:29:35 +0000

    Hopping waves.

---
+++ Assets/Hop.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hop : MonoBehaviour {
+	float hopTime = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hopTime > 0) {
+			hopTime -= Time.deltaTime;
+		}
+	}
+
+	public void Jump() {
+		if (hopTime <= 0) {
+			GetComponent<Rigidbody>().AddForce(Vector3.up * 200);
+			hopTime = 4.0f;
+		}
+	}
+}
--- Assets/WaveMaker.cs
+++ Assets/WaveMaker.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class WaveMaker : MonoBehaviour {
 
 	float waveProgress = 0.0f;
+	bool waveStarted = false;
+	Vector3 wavePos;
 
 	// Use this for initialization
 	void Start() {
@@ -15,12 +17,35 @@ public class WaveMaker : MonoBehaviour {
 	void Update () {
 		if (Input.GetButtonDown("Fire1")) {
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			if (Physics.Raycast(ray)) {
-				StartWave(transform.position);
+			Debug.Log("Click");
+			RaycastHit hitInfo;
+			if (Physics.Raycast(ray, out hitInfo)) {
+				StartWave(hitInfo.point);
+				Debug.Log("Hit");
+			}
+		}
+
+		if (waveStarted) {
+			waveProgress += 0.1f;
+			if (waveProgress > 5.0f) {
+				waveStarted = false;
+			}
+
+			Debug.Log(waveProgress);
+
+			foreach (Collider obj in Physics.OverlapSphere(wavePos, waveProgress)) {
+				Hop hop = obj.GetComponent<Hop>();
+				Debug.Log(obj);
+				if (hop != null) {
+					hop.Jump();
+				}
 			}
 		}
 	}
 
 	void StartWave(Vector3 pos) {
+		waveStarted = true;
+		this.waveProgress = 0.0f;
+		this.wavePos = pos;
 	}
 }
