c5b79b88dbc5fb1cb5c8dd21527acf8c06680e87
John Turner <git@norgg.org>
2017-01-21 15:39:17 +0000

    Add cheaty surf force.

--- Assets/Surf.cs
+++ Assets/Surf.cs
@@ -6,20 +6,29 @@ public class Surf : MonoBehaviour {
 
 	public float boyantForce = 0.4f;
 	public float boyantHeight = 2.0f;
+	public float angleClamp = 25;
+	public float flingForce = 20;
+
+	Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		GetComponent<Rigidbody>().AddForceAtPosition(Vector3.up * boyantForce, transform.position + transform.up * boyantHeight);
+		rb.AddForceAtPosition(Vector3.up * boyantForce, transform.position + transform.up * boyantHeight);
 
 		Vector3 angles = transform.eulerAngles;
-		angles.x = clampAngle(angles.x, 15);
-		angles.z = clampAngle(angles.z, 15);
+		angles.x = clampAngle(angles.x, angleClamp);
+		angles.z = clampAngle(angles.z, angleClamp);
 		transform.eulerAngles = angles;
+	}
+
+	public void Fling(Vector3 direction) {
+		Debug.Log("Flinging: " + direction);
+		rb.AddForce(direction.normalized * flingForce);
 	}
 
 	float clampAngle(float angle, float size) {
