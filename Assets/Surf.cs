b6601813dda25f705280fe10956e73048b606897
John Turner <git@norgg.org>
2017-01-21 14:00:55 +0000

    Boyancy force.

---
+++ Assets/Surf.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Surf : MonoBehaviour {
+
+	public float boyantForce = 0.4f;
+	public float boyantHeight = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		GetComponent<Rigidbody>().AddForceAtPosition(Vector3.up * boyantForce, transform.position + Vector3.up * boyantHeight);
+	}
+}
