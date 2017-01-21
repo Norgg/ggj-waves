9dfbba584932fc965622258b1d7ffb0055faa9ea
John Turner <git@norgg.org>
2017-01-21 15:18:50 +0000

    Make the ground move.

---
+++ Assets/Respawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+
+	Vector3 startPos;
+
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < -4) {
+			Vector3 newPos = transform.position;
+			newPos.y = startPos.y;
+			newPos.z += 25;
+			transform.position = newPos;
+		}
+	}
+}
