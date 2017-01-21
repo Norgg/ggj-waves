a220ff8a4a1ffae65dbedd0043be4c174d5a3497
John Turner <git@norgg.org>
2017-01-21 22:30:15 +0000

    Rainbow waves, fix quiff layer.

--- Assets/Hop.cs
+++ Assets/Hop.cs
@@ -6,10 +6,12 @@ public class Hop : MonoBehaviour {
 
 	public float hopStrength = 300;
 	bool grounded;
+	Material material;
 
 	// Use this for initialization
 	void Start () {
-		GetComponentInChildren<SkinnedMeshRenderer>().material.color = new Color(Random.value, Random.value, Random.value);
+		material = GetComponentInChildren<SkinnedMeshRenderer>().material;
+		material.color = new Color(Random.value, Random.value, Random.value);
 	}
 	
 	// Update is called once per frame
@@ -19,6 +21,9 @@ public class Hop : MonoBehaviour {
 	public void Jump(float strength) {
 		if (grounded) {
 			GetComponent<Rigidbody>().AddForce(Vector3.up * hopStrength * strength);
+			if (strength > 0.4f) {
+				material.color = new Color(Random.value, Random.value, Random.value);
+			}
 		}
 	}
 
