f51035144c1f841ad761b8c3784026ba81f99fab
John Turner <git@norgg.org>
2017-01-22 13:31:36 +0000

    Roaming speakers.

--- Assets/SpeakerRoam.cs
+++ Assets/SpeakerRoam.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class SpeakerRoam : MonoBehaviour {
 
+	public float speed = 1f;
+	Rigidbody rb;
+	float t;
+
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		t += 0.03f;
+		transform.position += new Vector3(speed * Mathf.Cos(t), 0, 0);
 	}
 }
