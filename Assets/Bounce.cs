679b733e334f51cb4869c63731bc7f541702d8ab
John Turner <git@norgg.org>
2017-01-22 12:32:34 +0000

    Singer stuff.

---
+++ Assets/Bounce.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour {
+	float t = 0;
+	Vector3 startPos;
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		startPos = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 pos = startPos;
+		t += 0.03f;
+		pos.y += Mathf.Sin(t);
+		pos.x += 1.5f * Mathf.Cos(4*t);
+		transform.localPosition = pos;
+
+		transform.LookAt(player.transform);
+	}
+}
