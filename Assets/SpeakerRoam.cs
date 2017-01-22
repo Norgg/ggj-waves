f1a23b7ceddf7983d5d0cb9945f9a2bbd7722fde
John Turner <git@norgg.org>
2017-01-22 13:40:49 +0000

    Less obnoxious speakers, stronger push force.

--- Assets/SpeakerRoam.cs
+++ Assets/SpeakerRoam.cs
@@ -5,17 +5,13 @@ using UnityEngine;
 public class SpeakerRoam : MonoBehaviour {
 
 	public float speed = 1f;
-	Rigidbody rb;
 	float t;
 
-	// Use this for initialization
 	void Start () {
-		rb = GetComponent<Rigidbody>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		t += 0.03f;
+		t += 0.003f;
 		transform.position += new Vector3(speed * Mathf.Cos(t), 0, 0);
 	}
 }
