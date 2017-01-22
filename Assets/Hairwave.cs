76214b833550bb5fa3bc7f366943fc862aee4551
John Turner <git@norgg.org>
2017-01-22 14:43:11 +0000

    Menu stuff.

---
+++ Assets/Hairwave.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hairwave : MonoBehaviour {
+
+	float t = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		t += 0.1f;
+		transform.rotation *= Quaternion.Euler(0, 0.8f * Mathf.Cos(t), 0);
+	}
+}
