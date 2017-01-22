10219f119c30b1a35d9e913b9fa8d3cdfbc3f8e9
John Turner <git@norgg.org>
2017-01-22 15:10:14 +0000

    Menu music.

--- Assets/PlayClick.cs
+++ Assets/PlayClick.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayClick : MonoBehaviour {
+	AudioSource loop;
+	AudioSource intro;
+
+	bool starting = false;
+	int startTime = 180;
 
 	// Use this for initialization
 	void Start () {
-		
+		loop = GetComponents<AudioSource>()[0];
+		intro = GetComponents<AudioSource>()[1];
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (starting) {
+			startTime--;
+			if (startTime <= 0) {
+				SceneManager.LoadScene("waves");	
+			}
+		}
 	}
 
 	public void LoadGame() {
-		SceneManager.LoadScene("waves");	
+		starting = true;
+		loop.Stop();
+		intro.Play();
 	}
 }
