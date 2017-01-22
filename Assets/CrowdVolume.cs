0874f4bf3cb9841f8cb99e1b75fb49243bdab756
John Turner <git@norgg.org>
2017-01-22 13:09:18 +0000

    Crowd volume, pulsating stuff.

---
+++ Assets/CrowdVolume.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowdVolume : MonoBehaviour {
+
+	public Rigidbody player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<AudioSource>().volume = 0.3f + player.velocity.magnitude / 10.0f;
+	}
+}
---
+++ Assets/Pulsate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pulsate : MonoBehaviour {
+	Vector3 startScale;
+	float t = 0;
+
+	// Use this for initialization
+	void Start () {
+		startScale = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		t += 0.15f;
+		transform.localScale = startScale * (1.0f + 0.01f * Mathf.Sin(t));
+	}
+}
