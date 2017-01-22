2b41bf8d48c89f3bf7b79b6460f65f4f4f80f1b6
John Turner <git@norgg.org>
2017-01-22 13:59:10 +0000

    Score

---
+++ Assets/Score.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+	public GameObject player;
+	float score;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		score += player.GetComponent<Rigidbody>().velocity.magnitude/10.0f;
+		GetComponent<TextMesh>().text = "" + (int)score;
+	}
+}
